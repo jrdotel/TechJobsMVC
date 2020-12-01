b7a67c8eabdb66e65bb9090a1d5a1029bf9a88cf
jrdotel <juliordotel@gmail.com>
2020-11-30 21:10:08 -0600

    Display Search Results

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +19,26 @@ namespace TechJobsMVC.Controllers
             return View();
         }
 
-        // TODO #3: Create an action method to process a search request and render the updated search view. 
+        // TODO #3: Create an action method to process a search request and render the updated search view.
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> jobs;
+
+            ViewBag.columns = ListController.ColumnChoices;
+
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, "all");
+                ViewBag.title = "All Jobs in Category: " + searchType;
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Jobs with " + ViewBag.columns[searchType] + ": " + searchTerm;
+            }
+
+            ViewBag.jobs = jobs;
+            return View("Index");
+        }
     }
 }
